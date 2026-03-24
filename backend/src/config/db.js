3a46d427cc177df7d997e754391d493c65ae1439
Ben Nguyen <jl.jimluv@gmail.com>
2026-03-24 14:14:03 +0700

    chore(db): add initial connection test for better startup feedback

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -8,11 +8,20 @@ const { Pool } = pg;
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 
 pool.on("connect", () => {
-  console.log(`PostgreSQL connected`);
+  console.log(`PostgreSQL pool connected`);
 });
 
 pool.on("error", (err) => {
   console.error(`PostgreSQL pool error:`, err);
 });
+
+pool
+  .query("SELECT 1")
+  .then(() => {
+    console.log("Connected to PostgreSQL database:", process.env.DATABASE_URL);
+  })
+  .catch((err) => {
+    console.error("Failed to connect to PostgreSQL:", err.message);
+  });
 
 export default pool;
